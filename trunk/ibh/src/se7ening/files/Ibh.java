eae60a69d16a41cce0df1901a6a203fa7f75849d
azserrata@gmail.com <azserrata@gmail.com@657563ee-cb94-02e9-3e45-ad3369cb2fcf>
2012-04-27 23:09:54 +0000

    License; Ibh structure; nodes add and search.

--- trunk/ibh/src/se7ening/files/Ibh.java
+++ trunk/ibh/src/se7ening/files/Ibh.java
@@ -1,6 +1,18 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (C) 2012 David Fernandes <davidfernandes@acm.org>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 
@@ -12,23 +24,47 @@ package se7ening.files;
  */
 public class Ibh {
     
-    private int valor;
-
+    private Node root;
+    
     public Ibh() {
-        valor = 0;
+        this.root = new Node("\\", "root");
     }
     
-    public Ibh(int _valor) {
-        valor = _valor;
+    //todo: import ibh text file
+    public Ibh(String pFilename) {
+        this.root = new Node("\\", "root");
     }
     
-    public int getValor() {
-        return this.valor;
+    public Node getRoot() {
+        return this.root;
     }
     
-    public void setValor(int _valor) {
-        this.valor = _valor;
+    // todo test
+    public Node getNode(String pPath) {
+        
+        if(this.root == null)
+            return null;
+        
+        Node ret = this.root;
+        
+        String[] keys = pPath.split("\\.");
+        
+        if(keys.length == 0)
+            return this.root;
+        
+        for(int i = 0; i < keys.length; i++) {
+            ret = ret.getNodeByKey(keys[i]);
+            if(ret == null)
+                return null;
+        }
+            
+        return ret;
     }
-    
 
+    // todo test
+    public Node getNodeCopy(String pPath) {
+        return this.getNode(pPath).clone();
+    }
+    
+    
 }
