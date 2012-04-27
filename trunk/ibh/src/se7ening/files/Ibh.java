8ce43666c187e34feb90656c602a71d83ea1f3e3
azserrata@gmail.com <azserrata@gmail.com@657563ee-cb94-02e9-3e45-ad3369cb2fcf>
2012-04-27 21:19:35 +0000

    First import.

---
+++ trunk/ibh/src/se7ening/files/Ibh.java
@@ -0,0 +1,34 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+package se7ening.files;
+
+/**
+ *
+ * @author David Fernandes <davidfernandes@acm.org>
+ */
+public class Ibh {
+    
+    private int valor;
+
+    public Ibh() {
+        valor = 0;
+    }
+    
+    public Ibh(int _valor) {
+        valor = _valor;
+    }
+    
+    public int getValor() {
+        return this.valor;
+    }
+    
+    public void setValor(int _valor) {
+        this.valor = _valor;
+    }
+    
+
+}
