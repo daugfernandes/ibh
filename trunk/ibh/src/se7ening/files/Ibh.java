f038c438e53fb5c27b6e4774273ce15dcae9e438
azserrata@gmail.com <azserrata@gmail.com@657563ee-cb94-02e9-3e45-ad3369cb2fcf>
2013-03-28 00:38:57 +0000

    Minor change

--- trunk/ibh/src/se7ening/files/Ibh.java
+++ trunk/ibh/src/se7ening/files/Ibh.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2012 David Fernandes <davidfernandes@acm.org>
+ * Copyright (C) 2012 David Fernandes <david.paiva.fernandes@gmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -81,7 +81,7 @@ public class Ibh {
             
             while ((strLine = br.readLine()) != null) {
 
-                // treat attributes as, for example
+                // treat attributes, for example
                 // tab length:  
                 // % tab 4
                 if(strLine.trim().startsWith("%")) {
